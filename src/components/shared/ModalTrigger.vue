5bc5568955707e26e3411cdc1c783c766ba24d01
obitrim <pevangelist516@gmail.com>
2020-03-22 19:06:15 +0000

    added modal trigger component

---
+++ src/components/shared/ModalTrigger.vue
@@ -0,0 +1,22 @@
+<template>
+    <button data-toggle="modal" :data-target="`#${modalId}`">
+        <slot />
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: 'button'
+        },
+
+        modalId: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
