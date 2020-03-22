724625a854b62e809d3f1f419ee7436d4d6883d1
obitrim <pevangelist516@gmail.com>
2020-03-22 19:11:43 +0000

    added file for firebase configuration data

---
+++ src/services/firebase-config.js
@@ -0,0 +1,10 @@
+
+export default {
+    apiKey: "AIzaSyDlb7JBNQ-hQChNsQHW7yx-sWALM7oVawU",
+    authDomain: "task-manager-b3bf6.firebaseapp.com",
+    databaseURL: "https://task-manager-b3bf6.firebaseio.com",
+    projectId: "task-manager-b3bf6",
+    storageBucket: "task-manager-b3bf6.appspot.com",
+    messagingSenderId: "461755978885",
+    appId: "1:461755978885:web:83438257bb966b10f345ec"
+};
