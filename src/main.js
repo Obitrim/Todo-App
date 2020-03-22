641162c27babaa04c9ba453dcd27a4778920c548
obitrim <pevangelist516@gmail.com>
2020-03-22 18:51:19 +0000

    setup firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import store from '@/store';
 import router from '@/router';
 import App from '@/views/App.vue';
 import plugins from '@/plugins.js';
+import firebaseConfig from '@/services/firebase-config.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// firebase
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Initializing Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 Vue.use(plugins);
 
