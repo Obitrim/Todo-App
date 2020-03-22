cc6fd58c9998ada859b69fac003da9719b2445ec
obitrim <pevangelist516@gmail.com>
2020-03-22 19:04:40 +0000

    added error label component

---
+++ src/components/shared/ErrorLabel.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="p-2 alert bg-danger error_label">
+        <small>
+            <i class="fa fa-times-circle mr-2"></i>
+            <slot/>
+        </small>
+    </div>
+</template>
