4ca41ccdca560913e2384a198c8608b47bbd0140
obitrim <pevangelist516@gmail.com>
2020-03-22 19:00:59 +0000

    added global component

--- src/components/global.js
+++ src/components/global.js
@@ -1,5 +1,11 @@
+import AppLoader from '@/components/shared/AppLoader.vue';
+import ErrorLabel from '@/components/shared/ErrorLabel.vue';
 import BaseButton from '@/components/buttons/BaseButton.vue';
+import ModalTrigger from '@/components/shared/ModalTrigger.vue';
 
 export default {
-    'base-button': BaseButton
+    'app-loader': AppLoader,
+    'base-button': BaseButton,
+    'error-label': ErrorLabel,
+    'modal-trigger': ModalTrigger,
 };
