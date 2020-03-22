75c3b7f70b5c3a1701944e8873a33f5168cf4508
obitrim <pevangelist516@gmail.com>
2020-03-22 19:03:41 +0000

    created app loader component

---
+++ src/components/shared/AppLoader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="app_loader position-fixed d-flex justify-content-center align-items-center"
+        v-if="$store.state.loader"
+        >
+        <div class="spinner"></div> <span class="text-white ml-2">Loading...</span>
+    </div>
+</template>
