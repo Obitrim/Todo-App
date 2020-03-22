566de67ef768ba2516a8ad52bd0aa6adbe3964ac
obitrim <pevangelist516@gmail.com>
2020-03-22 18:54:50 +0000

    i dont know what i did.

--- src/plugins.js
+++ src/plugins.js
@@ -3,7 +3,7 @@ import GlobalComponents from '@/components/global.js';
 
 export default {
     install(Vue){
-
+        
         //Registering global components
         for(let component in GlobalComponents){
             Vue.component(component, GlobalComponents[component]);
