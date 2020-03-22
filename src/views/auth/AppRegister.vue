2474d4631e75b128d2c6bfef95d33df76597bf4b
obitrim <pevangelist516@gmail.com>
2020-03-22 19:45:21 +0000

    binded input fields to properties

--- src/views/auth/AppRegister.vue
+++ src/views/auth/AppRegister.vue
@@ -8,6 +8,8 @@
                     name="username"
                     class="base_input"
                     id="username"
+                    v-model="credentials.username"
+                    required
                     />
                 <label for="text">Username</label>
             </div>
@@ -17,6 +19,8 @@
                     name="email"
                     class="base_input"
                     id="email"
+                    v-model="credentials.email"
+                    required
                     />
                 <label for="email">Email</label>
             </div>
@@ -26,6 +30,8 @@
                     name="password"
                     class="base_input"
                     id="password"
+                    v-model="credentials.password"
+                    required
                     autocomplete
                     />
                 <label for="password">Password</label>
@@ -36,3 +42,44 @@
         </form>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+             credentials: {
+                email: '',
+                password: '',
+                username: '',
+            },
+
+            errors: {
+                email: null,
+                password: null,
+                username: null,
+            }
+        }
+            
+    },
+
+    methods: {
+        // submit(){
+        //     if(this.validEmail() && this.validPassword()){
+        //         console.log("Login user");
+        //     }
+        // },
+
+        // validEmail(){
+        //     let emailValidationRes = new validator().validateEmail(this.credentials.email);
+        //     this.errors.email = !!emailValidationRes.error ? emailValidationRes.error : null;
+        //     return emailValidationRes.valid;
+        // },
+
+        // validPassword(){
+        //     let passwordValidationRes = new validator().validatePassword(this.credentials.password);
+        //     this.errors.password = !!passwordValidationRes.error ? passwordValidationRes.error : null;
+        //     return passwordValidationRes.ok;
+        // }
+    },  
+};
+</script>
