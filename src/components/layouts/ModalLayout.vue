bab16ea645b547be99ed0aa89545e195a46d3064
obitrim <pevangelist516@gmail.com>
2020-03-22 19:01:28 +0000

    added ModalLayout fcomponent

---
+++ src/components/layouts/ModalLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="modal fade" :id="modalId" aria-hidden="true" role="dialog">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content p-3  pt-5">
+                <slot/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modalId: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
